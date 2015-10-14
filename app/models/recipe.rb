4f559600529d1e94b7aad0071b0303d3ad23a0cb
ciaranc78 <ciaran.campbell@mail.com>
2015-10-14 17:02:36 +0100

    Add Image Uploader

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,17 @@
 class Recipe < ActiveRecord::Base
   belongs_to :chef
-   validates :name, length: { minimum: 5, maximum: 100 }
-   validates :summary, presence: true, length: { minimum: 10, maximum: 150 }
-   validates :description, presence: true, length: { minimum: 20, maximum: 200 }
-   validates :chef_id, presence: true
+  validates :name, length: { minimum: 5, maximum: 100 }
+  validates :summary, presence: true, length: { minimum: 10, maximum: 150 }
+  validates :description, presence: true, length: { minimum: 20, maximum: 200 }
+  validates :chef_id, presence: true
+  
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+  private 
+    def picture_size
+    	if picture.size > 5.megabytes
+    		errors.add(:picture, "Should be less than 5MB")
+    	end
+    end
 end
